d656cda50d4aab4ee5574348adf46abd04c826bb
harsh yadav <harshy110@gmail.com>
2026-08-13 17:42:25 +0530

    langchain agent with tool calling tavily

--- main.py
+++ main.py
@@ -1,39 +1,55 @@
 from dotenv import load_dotenv
+from typing import List
+from pydantic import BaseModel, Field
 
-load_dotenv()
 
 from langchain.agents import create_agent
+from langchain.agents.structured_output import ToolStrategy
 from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
 from langchain_tavily import TavilySearch
+# from tavily import TavilyClient
 
 from schemas import AgentResponse
+load_dotenv()
 
+class Source(BaseModel):
+    """ Schema for source used by agent"""
+    url: str = Field(description="The URL of the source")
+    
+    
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+    answer: str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(
+        default_factory=list, description="List of sources used to generate the answer"
+    )
+
+# tavily = TavilyClient()
+
+# @tool
+# def search(query:str) -> str:
+#     """
+#     Tool that searches over internet
+#     Args:
+#         query: The query to search for over the internet
+#     Returns:
+#         The search result
+#     """
+    
+#     print(f"Searching for {query}")
+#     return tavily.search(query = query)
+
+
+llm = ChatOpenAI(model="gpt-5.4-nano")
 tools = [TavilySearch()]
-llm = ChatOpenAI(model="gpt-4o")
-
-
-agent = create_agent(
-    model=llm,
-    tools=tools,
-    response_format=AgentResponse,
-)
-
+agent = create_agent(model=llm , tools = tools , response_format=ToolStrategy(AgentResponse))
 
 def main():
-    result = agent.invoke(
-        {
-            "messages": [
-                {
-                    "role": "user",
-                    "content": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
-                }
-            ]
-        }
-    )
-    # Access structured response from the agent
-    structured = result.get("structured_response", None)
-    print(structured if structured is not None else result)
-
+    print("creating a agent that is calling tool !!!")
+    result = agent.invoke({"messages":HumanMessage(content="what is the biggest panet in our solar system?")})
+    print(result)
 
 if __name__ == "__main__":
     main()
